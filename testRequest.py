62f0c327f04928d978c2deb69eff0fedb95c8f77
Mathias <mathias.blak@gmail.com>
2023-10-05 13:21:44 +0200

    new requirementes

--- testRequest.py
+++ testRequest.py
@@ -1,6 +1,14 @@
 print('hello, world')
 
 import requests
+from pyramid.view import view_config
+
+
+@view_config(route_name='home', renderer='templates/mytemplate.jinja2')
+def my_view(request):
+    return {'project': 'myproject'}
+
+
 
 response = requests.get('https://www.w3schools.com/python/python_sets.asp')
 print(response.text)
